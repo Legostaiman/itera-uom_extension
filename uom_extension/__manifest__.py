4d6cff9b8114f662b5e93d020817e996d1c3bebd
Aleksei <legostaiman@mail.ru>
2023-03-01 13:18:16 +0100

    review

--- uom_extension/__manifest__.py
+++ uom_extension/__manifest__.py
@@ -11,17 +11,12 @@
     'depends': [
         'uom',
         'base',
-        'sale',
     ],
     'data': [
         'views/test_account_view_move_form.xml',
         'views/uom_uom_views.xml',
-        'views/view_order_form.xml',
-        'views/view_stock_quant_tree_inventory_editable.xml',
         'views/product_template_tree_view.xml',
         'views/product_template_kanban_view.xml',
-        'views/view_warehouse_orderpoint_tree_editable.xml',
-        'views/view_warehouse_orderpoint_tree_editable_config.xml',
         'views/purchase_order_form.xml',
         'views/product_product_view.xml',
         'views/mrp_production_views.xml',
